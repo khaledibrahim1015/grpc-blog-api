a633dfd07fee06d499ae9a60c8b6816535129aa4
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-05 04:03:40 +0300

    Setup and Configuration and packages

---
+++ client/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+
+namespace client
+{
+    internal class Program
+    {
+        const string target = "127.0.0.1:50052";
+        static async Task Main(string[] args)
+        {
+            // Configuration To Connect on Server 
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The Client Connected Successfully");
+                }
+            });
+
+
+
+
+
+
+
+
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+
+namespace server
+{
+    internal class Program
+    {
+
+
+        const int port = 50052;
+        static void Main(string[] args)
+        {
+
+            Server server = null;
+
+            try
+            {
+
+                server = new Server()
+                {
+
+                    // Register services
+
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine("The Server is listening on the port " + port);
+
+
+
+
+                Console.ReadKey();
+
+
+
+            }
+            catch (IOException e )
+            {
+                Console.WriteLine("The Server is Failed to start : " + e.Message);
+                throw;
+                
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
